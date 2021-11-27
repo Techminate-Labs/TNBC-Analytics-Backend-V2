e9f91bc1bffb285b67ce5ddd4d37675317bd9117
SazidAhmed <sazidahmed.official@gmail.com>
2021-11-27 08:00:23 +0600

    faq model updated

--- apis/models/faq.py
+++ apis/models/faq.py
@@ -1,5 +1,4 @@
 from django.db import models
-from ckeditor.fields import RichTextField
 
 class FaqType(models.Model):
     title = models.CharField(max_length=255)
@@ -12,9 +11,8 @@ class FaqType(models.Model):
 
 class Faq(models.Model):
     faqType_id = models.ForeignKey(FaqType, on_delete=models.DO_NOTHING)
-    question = models.TextField()
-    # answer = models.TextField()
-    answer = RichTextField(blank=True, null=True)
+    question = models.TextField(blank=True, null=True)
+    answer = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
